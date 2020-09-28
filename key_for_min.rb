5a3c83bc5345c2992e72a7b2acc0794ca35a285a
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-28 19:48:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+<<<<<<< HEAD
   rkey = nil
   if name_hash == nil || name_hash == {}
     return nil
   else
+=======
+  if name_hash != nil
+    rkey = nil
+>>>>>>> fc1477318dba88b6862227327cec8e1c39cbaf4b
     name_hash.each do |key, value|
      if rkey == nil
         rkey = key
@@ -14,5 +19,10 @@ def key_for_min_value(name_hash)
       end
     end
     rkey.to_sym
+<<<<<<< HEAD
+=======
+  else
+    return nil
+>>>>>>> fc1477318dba88b6862227327cec8e1c39cbaf4b
   end
 end
